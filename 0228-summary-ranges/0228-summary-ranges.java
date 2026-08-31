690dc78f41b0d54781d01249572c2e15aa87aa38
SAI AMAR KODURU <kodurusaiamar@gmail.com>
2026-08-31 21:35:05 +0530

    Time: 3 ms (78.61%), Space: 43.2 MB (37.54%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+
+        int i = 0;
+
+        while (i < nums.length) {
+            int start = i;
+
+            while (i + 1 < nums.length && nums[i + 1] == nums[i] + 1) {
+                i++;
+            }
+
+            if (start == i) {
+                result.add(String.valueOf(nums[start]));
+            } else {
+                result.add(nums[start] + "->" + nums[i]);
+            }
+
+            i++;
+        }
+
+        return result;
+    }
+}
